ad9306c4485430707159497f90178e004aeb3767
Andi Moss <andi.r.moss@gmail.com>
2026-06-04 13:32:15 -0600

    naive implementation of list data structure for storing stitch definitions

--- src/CrochetISAmain.java
+++ src/CrochetISAmain.java
@@ -1,57 +1,90 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * 
  */
 public class CrochetISAmain {
-	/**
-	 * Creates a new stitch at the first insertion point that will create a flat fabric
-	 * @param insertionPoint: where the stitch will be inserted (top, between)
-	 * @param between: does this stitch create a new between
-	 * @param prevStitch: stitch currently on hook (previous to this new stitch)
-	 * @return: the newly created stitch
-	 */
-	Stitch createStitch(int insertionPoint, Stitch prevStitch) {
-		int width = 0;
-		//find Stitch width by tracing prevRow
-		Stitch[] prevRow = prevStitch.getPrevRow();
+	//enumerate insertion points
+	final static int TOP = 0;
+	final static int FRONT_LOOP = 1;
+	final static int BACK_LOOP = 2;
+	final static int FRONT_POST = 3;
+	final static int BACK_POST = 4;
+	final static int BETWEEN = 5;
 
-		if(prevRow.length > 1) {
-			//previous stitch was a decrease/spanned multiple stitches, so there isn't room to skip stitches
-			width = 1;
-		}
-		else {
-			Stitch current = prevStitch;
-			while(current.getPrevRow() != prevRow) {	//may need to add explicit comparator function
-				current = current.getBefore();
-				width++;
-			}
-		}
-		Stitch insertionStitch = prevStitch;
-		//assumes availability of insertion point
-		for(int i = 0; i < width; i++) {
-			insertionStitch = insertionStitch.getPrevRow()[0].getBefore();
-		}
-		Stitch row[] = {insertionStitch};
-		return new Stitch(prevStitch, row, insertionPoint, true);
-	}
-
-	Stitch chain(Stitch prevStitch){
-		//only create connection to previous stitch
-		Stitch chain = new Stitch(prevStitch, null, 0, false);
-		return chain;
-	}
-
-	Stitch newRow(Stitch prevStitch, int turningChains){
-		Stitch current = prevStitch;
-		for(int i = 0; i < turningChains; i++){
-			current = chain(current);
-		}
-		Stitch row[] = {prevStitch};
-		return new Stitch(current, row, 0, true);
-	}
+	//enumerate stitch types
+	final static int INSERT = 0;
+	final static int YO = 1;
+	final static int PT = 2;
+	final static int MOVE = 3;
+	final static int TURN = 4;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//key stitches
+		Integer[] ch = {YO, PT};
+		Integer[] sc = {INSERT, YO, PT, YO, PT, PT};
+		Integer[] hdc = {INSERT, YO, PT, YO, PT, PT, PT};
+		Integer[] dc = {INSERT, YO, PT, YO, PT, PT, YO, PT, PT};
+		ArrayList<Integer> CH = new ArrayList<Integer>(Arrays.asList(ch));
+		ArrayList<Integer> SC = new ArrayList<Integer>(Arrays.asList(sc));
+		ArrayList<Integer> HDC = new ArrayList<Integer>(Arrays.asList(hdc));
+		ArrayList<Integer> DC = new ArrayList<Integer>(Arrays.asList(dc));
 
+		//decrease stems
+		Integer[] decSC = {INSERT, YO, PT};
+		Integer[] decHDC = {INSERT, YO, PT};
+		Integer[] decDC = {INSERT, YO, PT, YO, PT, PT};
+
+		//increase (2 in one stitch)
+		ArrayList<Integer> incSC = new ArrayList<Integer>(Arrays.asList(sc));
+		incSC.addAll(Arrays.asList(sc));
+		ArrayList<Integer> incHDC = new ArrayList<Integer>(Arrays.asList(hdc));
+		incHDC.addAll(Arrays.asList(hdc));
+		ArrayList<Integer> incDC = new ArrayList<Integer>(Arrays.asList(dc));
+		incDC.addAll(Arrays.asList(dc));
+
+		//decrease (2 together)
+		ArrayList<Integer> dec2SC = new ArrayList<Integer>(Arrays.asList(decSC));
+		dec2SC.add(MOVE);
+		dec2SC.addAll(Arrays.asList(decSC));
+		dec2SC.add(YO);
+		dec2SC.add(PT);
+		dec2SC.add(PT);
+		dec2SC.add(PT);
+		dec2SC.add(PT);
+
+		ArrayList<Integer> dec2HDC = new ArrayList<Integer>(Arrays.asList(decHDC));
+		dec2HDC.add(MOVE);
+		dec2HDC.addAll(Arrays.asList(decHDC));
+		dec2HDC.add(YO);
+		dec2HDC.add(PT);
+		dec2HDC.add(PT);
+		dec2HDC.add(PT);
+		dec2HDC.add(PT);
+
+		ArrayList<Integer> dec2DC = new ArrayList<Integer>(Arrays.asList(decDC));
+		dec2DC.add(MOVE);
+		dec2DC.addAll(Arrays.asList(decDC));
+		dec2DC.add(YO);
+		dec2DC.add(PT);
+		dec2DC.add(PT);
+		dec2DC.add(PT);
+		dec2DC.add(PT);
+
+		// create stitch hash map
+		HashMap<String, ArrayList<Integer>> stitchMap = new HashMap<String, ArrayList<Integer>>();
+		stitchMap.put("ch", CH);
+		stitchMap.put("sc", SC);
+		stitchMap.put("hdc", HDC);
+		stitchMap.put("dc", DC);
+		stitchMap.put("sc2tog", dec2SC);
+		stitchMap.put("hdc2tog", dec2HDC);
+		stitchMap.put("dc2tog", dec2DC);
+		stitchMap.put("sc2inc", incSC);
+		stitchMap.put("hdc2inc", incHDC);
+		stitchMap.put("dc2inc", incDC);
 	}
-
 }
+
--- src/Stitch.java
+++ src/Stitch.java
@@ -2,28 +2,15 @@
  * Keeps track of insertion points
  */
 public class Stitch {
-	boolean top;
-	boolean between;
-	Stitch before;
-	Stitch prevRow[];
-	//Where was this inserted into? (front loop, back loop, top, between, fp, bp?)
-	int insertionPoint;
-	
-	Stitch(Stitch before, Stitch prevRow[], int insertionPoint, boolean between){
-		this.before = before;
-		this.prevRow = prevRow;
-		this.insertionPoint = insertionPoint;
-		top = true;
-		this.between = between;
+	int id;
+	int attached;
+	int insert;;
+	int type;
+
+	public Stitch(int id, int attached, int insert, int type) {
+		this.id = id;
+		this.attached = attached;
+		this.insert = insert;
+		this.type = type;
 	}
-	
-	Stitch getBefore() {return before;}
-	
-	Stitch[] getPrevRow() {return prevRow;}
-	
-	boolean getTop() {return top;}
-	void consumeTop() {top = false;}
-	boolean getBetween() {return between;}
-	void consumeBetween() {between = false;}
-	int getInsert() {return insertionPoint;}
 }
