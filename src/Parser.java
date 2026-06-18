ab9854dc92dde2145f90e909d65fa3061fc5170b
Andi Moss <andi.r.moss@gmail.com>
2026-06-18 10:01:02 -0600

    feature: multiple stitch syntax

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException; 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.Arrays;
 public class Parser {
@@ -21,16 +23,32 @@ public class Parser {
                 fileLines.add(scanner.nextLine());
             }
             scanner.close();
-            int i = 0;
+            int j = 0;
             for(String line : fileLines){
                 // TODO: add user defined stitch capability
                 line = line.trim();
                 line = line.replace(" ", "");
                 line = line.toLowerCase();
                 ArrayList<String> tokens = new ArrayList<String>(Arrays.asList(line.split(",")));
+                Pattern pattern = Pattern.compile("^(\\d+)(.*)");
+                for(int i = 0; i < tokens.size(); i++){
+                    String token = tokens.get(i);
+                    Matcher matcher = pattern.matcher(token);
+                    if(matcher.matches()){
+                        // Process the matched groups
+                        String count = matcher.group(1);
+                        String stitch = matcher.group(2);
+                        int countInt = Integer.parseInt(count);
+                        for(int k = 0; k < countInt; k++){
+                            tokens.add(i,stitch);
+                            i++;
+                        }
+                        tokens.remove(token);
+                    }
+                }
                 rows.add(tokens);
-                System.out.println("Row " + i + ": " + tokens.toString());
-                i++;
+                System.out.println("Row " + j + ": " + tokens.toString());
+                j++;
             }
         }
         catch(FileNotFoundException e){
